f24a17571bc5af9b5f225a13ee09983dbdf96597
Osmar Costa <osmar.costa.s@gmail.com>
2017-09-18 01:14:57 -0300

    feat: create component Bookshelf

---
+++ src/components/books/Bookshelf.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Bookshelf = (props) => (
+  <div className="bookshelf">
+    <h2 className="bookshelf-title">{ props.name}</h2>
+    <div className="bookshelf-books">
+      <ol className="books-grid">
+        {props.children.map((child, key) => (
+          <li key={key}>{child}</li>
+        ))}
+      </ol>
+    </div>
+  </div>
+);
+
+Bookshelf.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+export default Bookshelf;
