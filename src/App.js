cdce07cf7e3d4db8d0d5d649f0fe2be81ca3ba8f
Osmar Costa <osmar.costa.s@gmail.com>
2017-09-17 23:27:07 -0300

    feat: use BookApi to return books of user

--- src/App.js
+++ src/App.js
@@ -6,10 +6,24 @@ import * as BooksAPI from './BooksAPI';
 import './App.css';
 
 class BooksApp extends React.Component {
+  state = {
+    myBooks: [],
+    shelfs: {}
+  }
+  componentDidMount() {
+    this.setState({ shelfs: BooksAPI.shelfs });
+    BooksAPI.getAll().then(books => { 
+      this.setState({ myBooks: books });
+      console.log(books);
+    });
+  }
+
   render() {
     return (
       <div className='app'>
-        <Route exact path='/' component={HomeScene} />
+        <Route exact path='/' render={() => (
+          <HomeScene books={this.state.books} />
+        )} />
         <Route path='/search' component={SearchScene} />
       </div>
     )
