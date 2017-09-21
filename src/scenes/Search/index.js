6950b0fbd74ec0e282724dc751c2f88f9f66a4f4
Osmar Costa <p000osouza@prservicos.com.br>
2017-09-21 16:30:31 -0300

    feat: added handlers to search, move books, added list of books

--- src/scenes/Search/index.js
+++ src/scenes/Search/index.js
@@ -1,17 +1,35 @@
 import React, {Component} from 'react';
+import Book from './../../components/books/Book';
+import ButtonShelfChanger from './../../components/buttons/ButtonShelfChanger';
 
 class SearchScene extends Component{
+
   render() {
+    const { onSearchInput, books, shelfs, moveBook} = this.props;
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
           <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onInput={onSearchInput}
+            />
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {books.map(book => (
+              <Book key={book.id} book={book}>
+                <ButtonShelfChanger
+                  onChangeHandler={(e) => moveBook.bind(this, book)(e)}
+                  shelfs={shelfs}
+                  current={book.shelf || 'none'}
+                />
+              </Book>
+            ))}
+          </ol>
         </div>
       </div>
       )
