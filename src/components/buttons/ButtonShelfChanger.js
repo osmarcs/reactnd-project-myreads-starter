d1b341464cbd9c57a9326d1bbfcac72ccebbd641
Osmar Costa <osmar.costa.s@gmail.com>
2017-09-21 01:44:42 -0300

    refactor: remove static options tags, add props

--- src/components/buttons/ButtonShelfChanger.js
+++ src/components/buttons/ButtonShelfChanger.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const ButtonShelfChanger = () => (
+const ButtonShelfChanger = ({ shelfs, current, onChangeHandler }) => (
   <div className="book-shelf-changer">
-    <select>
-      <option value="none" disabled>Move to...</option>
-      <option value="currentlyReading">Currently Reading</option>
-      <option value="wantToRead">Want to Read</option>
-      <option value="read">Read</option>
-      <option value="none">None</option>
+    <select defaultValue={current} onChange={onChangeHandler}>
+      <option value="" disabled>Move to...</option>
+      {
+        Object.keys(shelfs).map(shelfKey => (
+            <option key={shelfKey} value={shelfKey}>
+              {shelfs[shelfKey]}
+           </option>
+        ))
+      }
     </select>
   </div>
 );
