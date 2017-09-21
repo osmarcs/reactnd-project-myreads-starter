96f35c7f18f731b71813e6009910b8d0b61778ce
Osmar Costa <p000osouza@prservicos.com.br>
2017-09-21 16:16:57 -0300

    fix: verify thumbnail exists

--- src/components/books/Book.js
+++ src/components/books/Book.js
@@ -5,7 +5,12 @@ import BookInfo from './BookInfo';
 const Book = ({ book, children }) => (
   <div className='book'>
     <div className='book-top'>
-      <BookAvatar image={book.imageLinks.thumbnail} alt={book.title} />
+      {book.imageLinks && book.imageLinks.thumbnail && (
+        <BookAvatar
+          image={book.imageLinks.thumbnail}
+          alt={book.title}
+        />
+      )}
       {children}
     </div>
     <BookInfo title={book.title} authors={book.authors} />
