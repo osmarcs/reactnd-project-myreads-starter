1010ccdb3994ed4b97e9c23ca8804e4117d3361e
Osmar Costa <p000osouza@prservicos.com.br>
2017-09-21 16:32:18 -0300

    refactor: create actions to load, search and move books, change routes to pass props do components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,84 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import _ from 'lodash';
+
 import HomeScene from './scenes/Home';
 import SearchScene from './scenes/Search';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
 
+const updateShelfOfBook = (book, shelf) => {
+  if (book.shelf === shelf) {
+    return book;
+  }
+  const cloneBook = _.cloneDeep(book);
+  cloneBook.shelf =  shelf;
+  return cloneBook;
+}
+
+const updateBooks = (currentBook, books) => {
+  return books.map((book) => {
+    if (currentBook.id === book.id) {
+      return updateShelfOfBook(book, currentBook.shelf);
+    }
+    return book;
+  });
+}
+
+const updateBooksShelf = (books, myBooks) => {
+  return books.map((book) => {
+    const bookIdx = myBooks.findIndex(myBook => book.id === myBook.id);
+    if (bookIdx > -1) {
+      return updateShelfOfBook(book, myBooks[bookIdx].shelf);
+    }
+    return book;
+  })
+};
+
 class BooksApp extends React.Component {
   state = {
     myBooks: [],
-    shelfs: {}
+    shelfs: {},
+    searchBooks: []
+  }
+  constructor(props) {
+    super(props);
+    this.searchInput = _.debounce(this.searchInput, 800);
+  }
+  searchInput(target) {
+    const bookSearch = target.value;
+    BooksAPI.search(bookSearch)
+      .then(books => {
+        if (books.error) {
+          books = [];
+        }
+        this.setState((state) => ({
+          searchBooks: updateBooksShelf(books, state.myBooks)
+        }))
+      });
+  }
+  moveBook(type, book, e) {
+    const shelf = e.target.value;
+    BooksAPI.update(book, shelf)
+      .then((result) => {
+        this.updateBooksState(type, book, shelf);
+      })
   }
   componentDidMount() {
     this.setState({ shelfs: BooksAPI.shelfs });
-    BooksAPI.getAll().then(books => { 
+    BooksAPI.getAll().then(books => {
       this.setState({ myBooks: books });
-      console.log(books);
+    });
+  }
+
+  updateBooksState(type, book, shelf) {
+    this.setState(({ searchBooks, myBooks }) => {
+      const bookUpdated = updateShelfOfBook(book, shelf);
+      const currUpdateBooks = (books) => (updateBooks(bookUpdated, books));
+      return {
+        myBooks: currUpdateBooks(myBooks),
+        searchBooks: currUpdateBooks(searchBooks)
+      };
     });
   }
 
@@ -23,12 +87,24 @@ class BooksApp extends React.Component {
     return (
       <div className='app'>
         <Route exact path='/' render={() => (
-          <HomeScene shelfs={shelfs} books={myBooks} />
+          <HomeScene
+            shelfs={shelfs}
+            books={myBooks}
+            moveBook={(book, e) => this.moveBook.bind(this,'myBooks')(book, e)}
+          />
         )} />
-        <Route path='/search' component={SearchScene} />
+        <Route path='/search' render={() => (
+          <SearchScene
+            books={this.state.searchBooks}
+            shelfs={shelfs}
+            onSearchInput={(e) => this.searchInput(e.target)}
+            moveBook={(book, e) => this.moveBook.bind(this,'search')(book, e)}
+          />
+        )}
+        />
       </div>
     )
   }
 }
 
-export default BooksApp
+export default BooksApp;
